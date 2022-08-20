6fe539221512c47f7d20b1b38f789d77d725669e
Abhishek Anand <abhirk247@gmail.com>
2022-08-20 19:45:17 +0530

    Finish Project

---
+++ script.js
@@ -0,0 +1,20 @@
+const feet=document.getElementById('feet');
+
+const inch=document.getElementById('inch');
+
+feet.addEventListener('input',function(){
+
+      let f=this.value;
+      let i=f*12;
+      inch.value=i;
+});
+
+inch.addEventListener('input',function(){
+
+  let f=this.value;
+  let i=f/12;
+  if(!Number.isInteger(i)){
+    i=i.toFixed(2);
+  };
+  feet.value=i;
+});
